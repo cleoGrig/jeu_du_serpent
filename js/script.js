8376901a10b10f4af7e435b8eb8865d88558717d
Cleo Grignon <cleogrig.747@gmail.com>
2020-11-20 09:38:11 -0500

    Affichage du pointage et creation d'une nouvelle partie

--- js/script.js
+++ js/script.js
@@ -2,19 +2,41 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
     //Le jeu
-    class Jeu{
-        constructor() {
-            console.log("creation du jeu");
+    class Jeu {
+        constructor(_idSvg, _idPointage) {
+            console.log("creation jeu");
+
+            this.s = Snap(_idSvg);
+            this.sortiePointage = document.querySelector(_idPointage);
+
+            this.grandeurCarre = 20;
+            this.grandeurGrille = 15;
         }
 
+        nouvellePartie() {
+            this.affichagePointage(1);
+
+            this.pomme = new Pomme();
+            this.serpent = new Serpent();
+        }
+
+
+        finPartie() {
+
+        }
+
+        affichagePointage(_lePointage) {
+            this.sortiePointage.innerHTML = _lePointage;
+        }
     }
+
 
     //Le serpent
     class Serpent{
-        constructor() {
+        constructor()
+        {
             console.log("creation serpent");
         }
-
     }
 
     //La pomme
@@ -25,5 +47,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     }
 
-    var unePartie=new Jeu();
+    var unePartie=new Jeu("#jeu", "#pointage");
+
+    var btnJouer=document.querySelector("#btnJouer");
+    btnJouer.addEventListener('click', nouvellePartie);
+
+    function nouvellePartie(){
+        unePartie.nouvellePartie();
+    }
 });
