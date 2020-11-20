0e00fa8ccbbcce8c1aab38f1af0d335184131ec9
Cleo Grignon <cleogrig.747@gmail.com>
2020-11-20 09:03:27 -0500

    Ajout de snap.svg et creation des classes pour le jeu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
+    //Le jeu
+    class Jeu{
+        constructor() {
+            console.log("creation du jeu");
+        }
 
+    }
+
+    //Le serpent
+    class Serpent{
+        constructor() {
+            console.log("creation serpent");
+        }
+
+    }
+
+    //La pomme
+    class Pomme{
+        constructor() {
+            console.log("creation pomme");
+        }
+
+    }
+
+    var unePartie=new Jeu();
 });
